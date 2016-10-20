7f49d5d87c5c43dfd6804828a5011242d67f4f49
4rrow <4rrow88@gmail.com>
2016-10-20 15:00:00 +1300

    Move  DateTime calculation to GigFormViewModel

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GigHub.Models;
+﻿using System;
+using GigHub.Models;
 using System.Collections.Generic;
 
 namespace GigHub.ViewModels
@@ -8,7 +9,12 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; }
+        public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime DateTime
+        {
+            get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); }
+        }
     }
 }
